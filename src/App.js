b9cf40ec2731db9b0c49163b74d5461922c8f7bc
Punith <kpunith8@gmail.com>
2018-08-05 18:39:48 +0530

    Adding transition to toggle

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component, Fragment } from 'react';
+import { Transition } from 'react-spring';
 import { ToggleRenderPropsChildren, Modal } from './utilities';
+import styled from 'styled-components';
 
 import './App.css';
 
@@ -20,9 +22,29 @@ class App extends Component {
             )
           }
         </ToggleRenderPropsChildren>
+        <MarginTop />
+        <ToggleRenderPropsChildren>
+          {
+            ({ enableToggle, toggleButton }) => (
+              <Fragment>
+                <button onClick={toggleButton}> Show / Hide </button>
+                <Transition from={{ opacity: 0 }} enter={{ opacity: 1 }} leave={{ opacity: 0 }}>
+                  {enableToggle && Header}
+                </Transition>
+              </Fragment>
+            )
+          }
+        </ToggleRenderPropsChildren>
+
       </div>
     );
   }
 }
+
+const Header = styles => <h2 style={{ ...styles }}>Show me</h2>;
+
+const MarginTop = styled.div`
+  margin-top: 10px;
+`;
 
 export default App;
