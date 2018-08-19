edb7e09d26caee099a05628e02e295bb7f69b986
Punith <kpunith8@gmail.com>
2018-08-19 19:20:19 +0530

    Removing  unwanter files

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 - styled-components
 
 - Add .env file to root folder and add this `NODE_PATH=src`, which makes src as root folder, and lets import thing in files specifying relatively
+
+- It covers, `Portal`, Creating re-usable `modal`
+- Usage of styled-components
+- Render props, render props as childern
--- src/utilities/index.js
+++ src/utilities/index.js
@@ -1,8 +1,7 @@
 import Modal from './Modal';
 import Portal from './Portal';
 import ToggleRenderPropsChildren from './ToggleRenderPropsChildren';
-import Icon from './Icon';
 import transition from './transition';
 import absolute from './position';
 
-export { Modal, Portal, ToggleRenderPropsChildren, Icon, transition, absolute };
+export { Modal, Portal, ToggleRenderPropsChildren, transition, absolute };
